df8909eaca4635da615bb6e812ff4e10fe1d8baa
summer <wsh6922@naver.com>
2024-11-09 19:12:02 +0900

    [refactor]: Board 엔티티 객체 전부 수정

--- src/main/java/com/namu/thenamu/domain/board/Board.java
+++ src/main/java/com/namu/thenamu/domain/board/Board.java
@@ -11,43 +11,43 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "development")
+@Table(name = "board")
 @Builder
 @DynamicInsert
 @DynamicUpdate
-public class Development {
+public class Board {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "dev_id", updatable = false)
+    @Column(name = "board_id", updatable = false)
     private Long id;
 
     @NotNull(message = "제목을 반드시 입력해야 합니다.")
-    @Column(name = "dev_content_title", nullable = false)
+    @Column(name = "board_content_title", nullable = false)
     private String title;
 
     @Lob
-    @Column(name = "dev_content", columnDefinition = "TEXT")
+    @Column(name = "board_content", columnDefinition = "TEXT")
     private String content;
 
     @CreationTimestamp
-    @Column(name = "dev_content_created_at", nullable = false, updatable = false)
+    @Column(name = "board_content_created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
-    @Column(name = "dev_content_updated_at")
+    @Column(name = "board_content_updated_at")
     private LocalDateTime updatedAt;
 
-    @Column(name = "dev_content_thumbnailImage")
+    @Column(name = "board_content_thumbnailImage")
     private String thumbnailImage;
 
     // TODO
     // Setter 메소드 대신에 비즈니스 메소드로 수정할 것
     // 객체 생성을 생성자 말고 정적팩토리메소드로 해볼 것
 
-    public Development() {}
+    public Board() {}
 
-    public Development(Long id, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public Board(Long id, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
         this.title = title;
         this.content = content;
